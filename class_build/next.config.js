3b956094ef20c7c489a334354c77f5b4a7e9f8a5
shinnr93 <shinnr93@gmail.com>
2021-12-22 13:33:27 +0900

    update docker

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
-}
+  generateBuildId: () => "com2me",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
+};
