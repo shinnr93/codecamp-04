1528851576fc5ee123a14a5c56a3bfb398de1b76
shinnr93 <shinnr93@gmail.com>
2021-12-23 23:59:08 +0900

    12/23 react-native

---
+++ freeboard_mobile_expo/App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+// import { WebView } from "react-native-webview";
+
+export default function App() {
+  // return <WebView source={{ uri: "https://naver.com" }} />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
